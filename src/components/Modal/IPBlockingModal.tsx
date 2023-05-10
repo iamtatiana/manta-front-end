9f627fb1d87307c9e6b83ed6c8a3bd1b1245d4f0
Jason <jasonintju@gmail.com>
2023-05-10 16:12:12 +0800

    fix: block content

--- src/components/Modal/IPBlockingModal.tsx
+++ src/components/Modal/IPBlockingModal.tsx
@@ -1,37 +1,17 @@
 import React, { useEffect } from 'react';
 import { useModal } from 'hooks';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Icon from 'components/Icon';
 
-const blockedCountries = ['US', 'CN', 'IR', 'CU', 'KP', 'SY', 'MM'];
-const blockedRegions = ['Crimea', 'Luhans\'k', 'Donets\'k'];
-const IPDATA_APIKEY = 'f47f1429b7dfb0d01a6d049b7cd283087b1b75fc3891f249d9c0919b';
-
 function IPBlockingModal() {
-  const { ModalWrapper, showModal, hideModal } = useModal({ closeDisabled: true });
+  const { ModalWrapper, showModal } = useModal({ closeDisabled: true });
   const navigate = useNavigate();
 
-
   useEffect(() => {
-    async function getUserGeolocation() {
-      if (!window.location.pathname.includes('/calamari/transact')) {
-        hideModal();
-        return;
-      }
-      const res = await axios.get(`https://api.ipdata.co?api-key=${IPDATA_APIKEY}`);
-      if (res.status === 200) {
-        const country_code = res?.data?.country_code;
-        const region = res?.data?.region;
-        if (blockedCountries.includes(country_code) || blockedRegions.includes(region)) {
-          showModal();
-        }
-      }
-    }
-    getUserGeolocation().catch(console.error);
+    showModal();
   }, []);
 
-  const onClickNav = (name:string, path: string) => {
+  const onClickNav = (name: string, path: string) => {
     if (name === 'NPO') {
       window.open(
         'https://npo.manta.network/calamari/sbt?utm_source=Application&utm_campaign=zkBAB&utm_medium=Organic&utm_term=Global&utm_content=MantaPay',
@@ -55,24 +35,30 @@ function IPBlockingModal() {
     {
       name: '$KMA Staking',
       path: '/calamari/stake'
-    },
+    }
   ];
 
   return (
     <ModalWrapper>
       <div className="w-140 bg-fourth -mx-6 -my-4 rounded-lg p-6">
-        <div className="text-xl leading-6">MANTAPAY IS NOT AVAILABLE IN YOUR LOCATION</div>
-        <div className="text-sm text-secondary leading-5 my-4">
-          It appears that this connecting is from a prohibited region (United States, China, Iran, Cuba, North Korea, Syria, Myanmar (Burma), the regions of Crimea, Donetsk or Luhansk). If you're using a VPN, try disabling it.
+        <div className="text-xl leading-6">
+          MANTAPAY IS NOT AVAILABLE IN YOUR LOCATION
         </div>
-        {
-          navs.map(({ name, path }) => (
-            <button key={path} onClick={() => onClickNav(name, path)} className="mt-6 flex items-center justify-between px-4 border border-solid border-white border-opacity-10 bg-white bg-opacity-5 w-full h-12 cursor-hover text-sm cursor-pointer rounded-lg">
-              <span>{name}</span>
-              <Icon className="w-2 h-2" name="right" />
-            </button>)
-          )
-        }
+        <div className="text-sm text-secondary leading-5 my-4">
+          It appears that this connecting is from a prohibited region (United
+          States, China, Iran, Cuba, North Korea, Syria, Myanmar (Burma), the
+          regions of Crimea, Donetsk or Luhansk). If you're using a VPN, try
+          disabling it.
+        </div>
+        {navs.map(({ name, path }) => (
+          <button
+            key={path}
+            onClick={() => onClickNav(name, path)}
+            className="mt-6 flex items-center justify-between px-4 border border-solid border-white border-opacity-10 bg-white bg-opacity-5 w-full h-12 cursor-hover text-sm cursor-pointer rounded-lg">
+            <span>{name}</span>
+            <Icon className="w-2 h-2" name="right" />
+          </button>
+        ))}
       </div>
     </ModalWrapper>
   );
--- src/pages/SendPage/index.tsx
+++ src/pages/SendPage/index.tsx
@@ -1,11 +1,51 @@
+import { React, useState, useEffect } from 'react';
 import IPBlockingModal from 'components/Modal/IPBlockingModal';
 import Navbar from 'components/Navbar';
 import PageContent from 'components/PageContent';
+import axios from 'axios';
 import { SendContextProvider } from './SendContext';
 import SendForm from './SendForm';
 import { PrivateTxHistoryContextProvider } from './privateTxHistoryContext';
 
+const blockedCountries = ['US', 'CN', 'IR', 'CU', 'KP', 'SY', 'MM'];
+const blockedRegions = ['Crimea', "Luhans'k", "Donets'k"];
+const IPDATA_APIKEY =
+  'f47f1429b7dfb0d01a6d049b7cd283087b1b75fc3891f249d9c0919b';
+
 const SendPage = () => {
+  const [shouldBlockIP, setShouldBlockIP] = useState(false);
+  const [checkingIP, setCheckingIP] = useState(true);
+
+  useEffect(() => {
+    async function getUserGeolocation() {
+      const res = await axios.get(
+        `https://api.ipdata.co?api-key=${IPDATA_APIKEY}`
+      );
+      if (res.status === 200) {
+        const country_code = res?.data?.country_code;
+        const region = res?.data?.region;
+        if (
+          blockedCountries.includes(country_code) ||
+          blockedRegions.includes(region)
+        ) {
+          setShouldBlockIP(true);
+        }
+      }
+    }
+    setShouldBlockIP(true);
+    getUserGeolocation()
+      .catch(console.error)
+      .finally(() => setCheckingIP(false));
+  }, []);
+
+  if (checkingIP) {
+    return null;
+  }
+
+  if (shouldBlockIP) {
+    return <IPBlockingModal />;
+  }
+
   return (
     <SendContextProvider>
       <PrivateTxHistoryContextProvider>
@@ -13,7 +53,6 @@ const SendPage = () => {
         <PageContent>
           <SendForm />
         </PageContent>
-        <IPBlockingModal />
       </PrivateTxHistoryContextProvider>
     </SendContextProvider>
   );
