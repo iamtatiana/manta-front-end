3e639e6776b58c8e2701599cf43c2204eff727b5
Kevin Gislason <kevingislason@gmail.com>
2023-05-17 21:41:01 -0400

    cleanup

--- src/pages/SendPage/SendToForm/FeeDisplay.tsx
+++ src/pages/SendPage/SendToForm/FeeDisplay.tsx
@@ -1,13 +1,13 @@
-// @ts-nocheck
 import React from 'react';
 import { useSend } from '../SendContext';
 
 const FeeDisplay = () => {
   const { feeEstimate } = useSend();
+  const feeText = feeEstimate?.toDisplayString() || '-';
   return (
     <div className="flex justify-between items-center inline w-full pt-3">
       <div className="text-white">Gas fee</div>
-      <div className="text-white">{feeEstimate?.toDisplayString() || '-'}</div>
+      <div className="text-white">{feeText}</div>
     </div>
   );
 };
